c650c12eb8b63be98244846c3d750a8649f93fac
iboros1 <iborosppc@gmail.com>
2016-10-31 21:00:17 +0200

    Adding Config file

--- Connect.py
+++ Connect.py
@@ -1,19 +1,9 @@
 from ftplib import FTP
-from pip._vendor.distlib.compat import raw_input
+import config
 
-port = 'Default'
-host = '192.168.1.1'
-user = raw_input('User:')
-password = raw_input('Password:')
-
-CamFolderList = []
-SelectedFolder = 1  # "0" folder 1 , "1" folder Home  , "2" folder Pata
-FolderContent = []
-
-ftp = FTP(host=host, user=user, passwd=password, timeout=20)   # connect to host, default port
-print('Current ip is ' + host + ':' + port)
-ftp.retrlines('LIST')     # list directory contents
-
+ftp = FTP(host=config.host, user=config.user, passwd=config.password, timeout=20)  # connect to host, default port
+print('Current ip is ' + config.host + ':' + config.port)
+ftp.retrlines('LIST')  # list directory contents
 
 # go to Camera Root Folder
 Root = ftp.nlst()
@@ -22,19 +12,18 @@ MainFolder = ftp.nlst()
 CameraRoot = MainFolder[0]  # "0" Camera Folder
 ftp.cwd(CameraRoot)
 
-#Create list from cam folders
+# Create list from cam folders
 CamFolderList = ftp.nlst()
 print(CamFolderList)
 
-#Go to Cam folder "Home"
-ftp.cwd(CamFolderList[SelectedFolder])
+# Go to Cam folder "Home"
+ftp.cwd(CamFolderList[config.SelectedFolder])
 print(ftp.pwd())
 
-#create list with selected folder content
+# create list with selected folder content
 FolderContent = ftp.nlst()
 print('Files in folder: ' + str(len(FolderContent)))
 
 ftp.delete(FolderContent[0])
-
 
 ftp.quit()
---
+++ config.py
@@ -0,0 +1,16 @@
+from pip._vendor.distlib.compat import raw_input
+
+# [Login Info]
+
+port = 'Default'
+host = 'boros.ddns.net'
+user = raw_input('User:')
+password = raw_input('Password:')
+
+# [Folder List]
+
+SelectedFolder = 1  # "0" folder 1 , "1" folder Home  , "2" folder Pata
+
+# [Folder list placeholder]
+CamFolderList = []
+FolderContent = []
