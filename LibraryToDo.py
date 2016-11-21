77f5e0b1d71d9e867f02df218e6c4a2b103f20a9
iboros1 <iborosppc@gmail.com>
2016-11-21 22:23:23 +0200

    ITT WORKS 2

--- LibraryToDo.py
+++ LibraryToDo.py
@@ -1,5 +1,6 @@
 from config import (Host, User, Password, Port)
 from ftplib import FTP
+import datetime
 
 
 class RunFtp:
@@ -17,5 +18,22 @@ class RunFtp:
     def get_current_folders_list(self):
         # Create list from cam folders
         return self.ftp.nlst()
+    def delete(target):
+        self.ftp.delete(target)
+
+    def if_date_date_older(image):
+        now = datetime.datetime.now()
+        target_date = datetime.timedelta(days=20)
+        date = now - target_date
+        date = int('{0}{1}{2}{3}{4}{5}'.format(now.year, now.month, now.day, now.hour, now.minute, now.second))
+        ftp = FTP(host='boros.ddns.net', user='xxxxxxx', passwd='xxxxxxx')
+        print(ftp.getwelcome())
+        ftp.cwd("/Free4All/Camera/Home/20161102")
+        image_date = ftp.sendcmd('MDTM ' + image)
+        image_date = int(image_date[3:])
+        print(image_date)
+        print(date)
+        if image_date < date:
+            delete()
 
 
