b7b9eb5536227adff44827e4f37ebe046c567aef
Istvan Boros <istvan.boros@3pillarglobal.com>
2016-11-24 12:12:36 +0200

    All work added comments

--- Connect.py
+++ Connect.py
@@ -1,61 +1,16 @@
 from config import *
 from LibraryToDo import RunFtp
-import os, time
-
-
 
 runner = RunFtp()
 runner.login()
-log = open('log.txt', 'a') # When "a" Append to log.txt , if "w" delete all and add
 
+#Connect to ftp server
 ftp = runner.get_ftp_connection()
+
 # go to Camera Root Folder
 ftp.cwd(CameraRoot)
-cam_folder_list = runner.get_current_folders_list()
 
-log.write("Display Folder List" + str(cam_folder_list))
-nr_of_images_in_root = 1
-
-
-
-log.write("\n \n \n \n ")
-log.write("Time: " + time.strftime("%c") + "\n \n \n")
-
-now = time.time()
+#Go troth all the files
 runner.browse_files()
-# Go go through  all folders from "cam_folder_list"
-for List in cam_folder_list:
-    ftp.cwd(List)
-    #print("Current Folder is: " + str(ftp.pwd()))
-    log.write("Current Folder is: " + str(ftp.pwd()) + "\n")
-    # If Folder is empty Print Else
-    x = ftp.nlst()
-    if x != []:
-        for sub_List in x:
-            try:
-                ftp.cwd(sub_List)
-                print(ftp.pwd())
-                log.write("\n \n " + "Present Working directory: " + str(ftp.pwd()) + "\n \n \n")
-                print(ftp.retrlines('LIST'))
-                for image in ftp.nlst():
-                   log.write(image + "\n")
-                   runner.older_file_delete(image)
 
-            except:
-                nr_of_images_in_root += 1
-                ftp.cwd("../")
-                for List in cam_folder_list:
-                    ftp.cwd(List)
-                    x = ftp.nlst()
-                    if x != []:
-                        for image in ftp.nlst():
-                            log.write(image + "\n")
-                            runner.older_file_delete(image)
-    else:
-        log.write("Goooooool" + "\n")
-
-log.write("Nr of images in root: " + str(nr_of_images_in_root))
-
-# ftp.delete(FolderContent[0])
-log.close()
 ftp.quit()
--- LibraryToDo.py
+++ LibraryToDo.py
@@ -1,6 +1,6 @@
 from config import (Host, User, Password, Port, DDay)
 from ftplib import FTP, error_perm
-import datetime
+import datetime, time
 import os
 
 
@@ -8,51 +8,66 @@ class RunFtp:
     def __init__(self):
         self.ftp = None
 
+    # login to given ftp server
     def login(self):
         self.ftp = FTP(host=Host, user=User, passwd=Password, timeout=20)  # connect to host, default port
         print('Current ip is ' + Host + ':' + Port + "\n" + self.ftp.getwelcome())
         self.ftp.retrlines('LIST')  # list directory contents
 
+    # get ftp server
     def get_ftp_connection(self):
         return self.ftp
 
+    # Create list from cam folders
     def get_current_folders_list(self):
-        # Create list from cam folders
         return self.ftp.nlst()
 
+    # Delete files
     def delete_file(self, target):
-      #  self.ftp.delete(target)
-      print('1')
+        self.ftp.delete(target)
 
+    # Get file last update date and  gompare with date if older delete
     def older_file_delete(self, image):
         # Delete Filers older then "DDay" from Config.py
         date = datetime.datetime.now() - datetime.timedelta(days=DDay)
-        #format date to yyyymmddhhmmss %02 is used to display leading 0
+        # format date to yyyymmddhhmmss %02 is used to display leading 0
         date = int('%s%02d%02d%02d%02d%02d' % (date.year, date.month, date.day, date.hour, date.minute, date.second))
-        image = os.path.basename(image) # Remove folders from string
+        image = os.path.basename(image)  # Remove folders from string
         try:
-            image_date = self.ftp.sendcmd('MDTM ' + image) # get image last update date
+            image_date = self.ftp.sendcmd('MDTM ' + image)  # get image last update date
         except error_perm:
             self.ftp.cwd('../')
             image_date = self.ftp.sendcmd('MDTM ' + image)
-        image_date = int(image_date[3:]) #remove 213 response from string
-        if image_date < date: # if image older log to file and Delete
+        image_date = int(image_date[3:])  # remove 213 response from string
+        # if image older log to file and Delete else only log to file
+        if image_date < date:
             self.delete_file(image)
             self.log_deleted_files(image)
+        else:
+            self.log_remaining_files(image)
 
+    # Log deleted filename to log_delete.txt
     @staticmethod
     def log_deleted_files(image):
-        log = open('log_deleted.txt', 'a')  # When "a" Append to log.txt , if "w" delete all and add
+        log = open('log_deleted.txt', 'a')  # When "a" Append to log
         log.write("Deleted image:" + image + "\n")
         log.close()
 
+    # Log remaining filename to log.txt
+    @staticmethod
+    def log_remaining_files(image):
+        log = open('log.txt', 'a')  # When "a" Append to log.txt
+        log.write("\n \n \n \n ")
+        log.write("Time: " + time.strftime("%c") + "\n \n \n")
+        log.write("Image on server:" + image + "\n")
+        log.close()
+
+    # Browse all folders and files if ends with .jpg check if old else restart from top
     def browse_files(self):
         for List in self.get_current_folders_list():
             basename = os.path.basename(List)
-            print("Directorul de acum", basename)
             if basename.endswith('.jpg'):
                 self.older_file_delete(basename)
             else:
                 self.ftp.cwd(List)
                 self.browse_files()
-
